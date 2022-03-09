13959d43752ae695488ffd5c30650d6d051fc5e4
Simon Ericsson <simon@krlsg.se>
2022-03-09 09:44:21 +0100

    Added config file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const config = require("./config.json")
+
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const PORT = 5000;
+const PORT = config.port;
 
 const routes = require('./routes/routes.js');
 
