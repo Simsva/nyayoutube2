8d1e0aa194340301614cf974c643e2ab0d9c1d72
Gustav Jakobsson <gustav.jakobsson@abbgymnasiet.se>
2022-10-25 13:40:53 +0200

    fixed synd

--- routes/routes.js
+++ routes/routes.js
@@ -131,14 +131,13 @@ router.get('/sync', async(req, res) => {
         } 
         files_data = []
         const data_fname = path.join(__dirname, '/../data/data.json');
-        let data = await readJsonFromFile(data_fname);
+        let data = {videos: []};
         files.forEach(async(file) => {
             const data_fname = path.join(__dirname, '/../data/data.json');
-            let data = await readJsonFromFile(data_fname);
             let obj = {title: file.split('.')[0], loves: 0, hates: 0, comments: []};
             data["videos"].push(obj);
-            await writeJsonToFile(data, data_fname);
         });
+        await writeJsonToFile(data, data_fname);
         res.send(files_data);
     });
 });
